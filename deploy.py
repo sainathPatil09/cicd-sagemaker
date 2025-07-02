d5711de23771e9b6e9dc114470760c628c729d9e
sainathPatil09 <sainathpatilpanda@gmail.com>
2025-07-02 16:09:45 +0530

    test

--- deploy.py
+++ deploy.py
@@ -5,7 +5,7 @@ from sagemaker.sklearn.model import SKLearnModel
 from sagemaker import Session
 
 s3_model_path = "s3://models-sagemaker-iris/model/model.tar.gz"  # Replace with your S3 path
-role = "arn:aws:iam::396608790362:role/service-role/AmazonSageMaker-ExecutionRole-20250621T181246"  # Replace this
+role = "arn:aws:iam::396608790362:role/mySageMakerrole"  # Replace this
 
 sagemaker_session = Session()
 
@@ -20,3 +20,4 @@ model = SKLearnModel(
 predictor = model.deploy(instance_type="ml.t2.medium", initial_instance_count=1)
 
 print("Model deployed at endpoint:", predictor.endpoint_name)
+
